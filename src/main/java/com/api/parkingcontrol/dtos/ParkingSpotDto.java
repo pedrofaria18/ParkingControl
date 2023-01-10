ffd62b367a19f1cdd13fa2abf82537404676c96a
pedro <pedrof.andrade16@gmail.com>
2023-01-09 21:05:24 -0300

    First Commit

---
+++ src/main/java/com/api/parkingcontrol/dtos/ParkingSpotDto.java
@@ -0,0 +1,41 @@
+package com.api.parkingcontrol.dtos;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDateTime;
+
+public class ParkingSpotDto {
+    @NotBlank
+    private String parkingSpotNumber;
+    @NotBlank
+    @Size(max = 7)
+    private String licensePlateCar;
+    @NotBlank
+    private String brandCar;
+    @NotBlank
+    private String modelCar;
+    @NotBlank
+    private String colorCar;
+    @NotBlank
+    private String responsibleName;
+    @NotBlank
+    private String apartment;
+    @NotBlank
+    private String block;
+
+    @Override
+    public String toString() {
+        return "ParkingSpotDto{" +
+                "parkingSpotNumber='" + parkingSpotNumber + '\'' +
+                ", licensePlateCar='" + licensePlateCar + '\'' +
+                ", brandCar='" + brandCar + '\'' +
+                ", modelCar='" + modelCar + '\'' +
+                ", colorCar='" + colorCar + '\'' +
+                ", responsibleName='" + responsibleName + '\'' +
+                ", apartment='" + apartment + '\'' +
+                ", block='" + block + '\'' +
+                '}';
+    }
+}
